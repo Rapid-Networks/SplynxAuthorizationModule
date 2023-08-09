18c4715a2a3240eacca94fd5c764176d84ff3b17
Armin <armin@rapidnetworks.co.za>
2023-08-09 14:34:41 +0200

    licence temp

--- src/components/server/server.ts
+++ src/components/server/server.ts
@@ -1,47 +1,58 @@
 import Fastify from 'fastify';
 import { config } from '../../libraries/env/convict.js';
+import dbClient from '../db/redis.js';
 
-export function initializeServer(environment: string): Fastify.FastifyInstance {
-  // Logger configuration for current environment:
-  const loggerEnv: Record<string, any> = {
-    development: {
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          translateTime: 'HH:MM:ss Z',
-          ignore: 'pid,hostname',
-        },
+// Logger configuration for current environment:
+const loggerOptions: Record<string, any> = {
+  development: {
+    name: 'dev',
+    transport: {
+      target: 'pino-pretty',
+      level: 'trace',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
       },
     },
-    production: true,
-    test: false,
-  };
+  },
+  production: {
+    name: 'production',
+    level: 'info',
+  },
+  test: false,
+};
 
-  const fastify = Fastify({
-    logger: loggerEnv[environment] ?? true,
-  });
+export const fastify = Fastify({
+  logger: loggerOptions[config.get('env')] ?? true,
+});
 
-  // Routes
-  fastify.get('/authentication', async (_request, reply) => {
-    reply.send('latest splynx header').code(200);
-  });
+// Routes
+/**
+ * Base request to route, returns
+ */
+fastify.get('/', async (_request, reply) => {
+  reply.send('latest splynx header').code(200);
+});
+fastify.get('/authentication', async (_request, reply) => {
+  // need special logger options for requests
+  reply.send('latest splynx header').code(200);
+});
 
-  fastify.get('/authentication/token', async (_request, reply) => {
-    reply.send('raw token data').code(200);
-  });
+fastify.get('/authentication/token', async (_request, reply) => {
+  reply.send('raw token data').code(200);
+});
 
-  fastify.get('/authentication/permissions', async (_request, reply) => {
-    reply.send('permissions').code(200);
-  });
+fastify.get('/authentication/permissions', async (_request, reply) => {
+  reply.send('permissions').code(200);
+});
 
-  fastify.get('/logs', async (_request, reply) => {
-    reply.send('logs endpoint').code(200);
-  });
+fastify.get('/logs', async (_request, reply) => {
+  reply.send('logs endpoint').code(200);
+});
 
+export function startServer(): void {
   // running server
   fastify.listen({ port: config.get('port') }, (error, _address) => {
     if (error) throw new Error();
   });
-
-  return fastify;
 }
