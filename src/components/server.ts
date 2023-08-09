65d264c926b7b8eabc5dd94472417bf44a3e45f6
Armin <armin@rapidnetworks.co.za>
2023-08-10 01:43:09 +0200

    working routes

--- src/components/server.ts
+++ src/components/server.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import config from '../libraries/environment.js';
 import dbClient from './database.js';
+import { AppError } from '../libraries/errors.js';
 
 // Logger configuration for current environment:
 const loggerOptions: Record<string, any> = {
@@ -32,26 +33,62 @@ export const fastify = Fastify({
 fastify.get('/', async (_request, reply) => {
   reply.send(await dbClient.get('header:latest')).code(200);
 });
+/**
+ * Return full token from database, without permissions
+ */
+fastify.get('/token', async (_request, reply) => {
+  const tokenWithoutPermissions = {
+    access_token: await dbClient.hGet('token:latest', 'access_token'),
+    refresh_token: await dbClient.hGet('token:latest', 'refresh_token'),
+    access_token_expiration: await dbClient.hGet(
+      'token:latest',
+      'access_expiry',
+    ),
+    refresh_token_expiration: await dbClient.hGet(
+      'token:latest',
+      'refresh_expiry',
+    ),
+  };
 
-fastify.get('/authentication', async (_request, reply) => {
-  // need special logger options for requests
-  reply.send('latest splynx header').code(200);
+  reply.send(tokenWithoutPermissions).code(200);
 });
+/**
+ * Return full token from database, including permissions
+ */
+fastify.get('/token/raw', async (_request, reply) => {
+  const dump = await dbClient.hGetAll('token:latest');
+  const permissionDump = JSON.parse(dump.permissions);
 
-fastify.get('/authentication/token', async (_request, reply) => {
-  reply.send('raw token data').code(200);
+  const parsedToken = {
+    access_token: dump.access_token,
+    refresh_token: dump.refresh_token,
+    access_token_expiration: dump.access_expiry,
+    refresh_token_expiration: dump.refresh_expiry,
+    permissions: { ...permissionDump },
+  };
+
+  reply.send(parsedToken).code(200);
 });
-
-fastify.get('/authentication/permissions', async (_request, reply) => {
-  reply.send('permissions').code(200);
+/**
+ * Return permission list from database
+ */
+fastify.get('/permissions', async (_request, reply) => {
+  const permissionDump = await dbClient.hGet('token:latest', 'permissions');
+  if (permissionDump === undefined) {
+    throw new AppError(
+      'APP_ERROR',
+      'Permissions not found on token object',
+      new Error(),
+    );
+  }
+  reply.send(JSON.parse(permissionDump)).code(200);
 });
-
+// @TODO - implement log dumping and streaming
 fastify.get('/logs', async (_request, reply) => {
   reply.send('logs endpoint').code(200);
 });
 
 export function startServer(): void {
-  // running server
   fastify.listen({ port: config.get('server.port') }, (error, _address) => {
     if (error) throw new Error();
   });
